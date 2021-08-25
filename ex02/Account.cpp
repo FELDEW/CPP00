145b3fbe627cd3c3161c9e0ad81ff2ef319d9b62
Hazel Buck <hbuck@mi-v1.msk.21-school.ru>
2021-08-25 16:50:01 +0300

    fixed destructor

--- ex02/Account.cpp
+++ ex02/Account.cpp
@@ -43,6 +43,7 @@ Account::Account( int initial_deposit )
 	this->_amount = initial_deposit;
 	this->_accountIndex = _nbAccounts;
 	_nbAccounts++;
+	_totalAmount += this->_amount;
 	this->_nbDeposits = 0;
 	this->_nbWithdrawals = 0;
 	_displayTimestamp();
@@ -51,9 +52,29 @@ Account::Account( int initial_deposit )
 	std::cout << "created" << std::endl;
 	return ;
 }
+
+Account::Account( void )
+{
+	this->_amount = 0;
+	this->_accountIndex = _nbAccounts;
+	_nbAccounts++;
+	_totalAmount += this->_amount;
+	this->_nbDeposits = 0;
+	this->_nbWithdrawals = 0;
+	_displayTimestamp();
+	std::cout << "index:" << this->_accountIndex << ";";
+	std::cout << "amount:" << this->_amount << ";";
+	std::cout << "created" << std::endl;
+	return ;
+}
+
 Account::~Account( void )
 {
 	_displayTimestamp();
+	_nbAccounts--;
+	_totalNbWithdrawals -= this->_nbWithdrawals;
+	_totalNbDeposits -= this->_nbDeposits;
+	_totalAmount -= this->_amount;
 	std::cout << "index:" << this->_accountIndex << ";";
 	std::cout << "amount:" << this->_amount << ";";
 	std::cout << "closed" << std::endl;
